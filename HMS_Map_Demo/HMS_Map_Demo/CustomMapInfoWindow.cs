f99c69660647fe37d5170909cd10262f8359f47c
omar hmead <omernasery@gmail.com>
2020-05-11 17:56:42 +0300

    Full Demo

---
+++ HMS_Map_Demo/HMS_Map_Demo/CustomMapInfoWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Huawei.Hms.Maps;
+using Com.Huawei.Hms.Maps.Model;
+
+namespace HMS_Map_Demo
+{
+    class CustomMapInfoWindow : Java.Lang.Object, HuaweiMap.IInfoWindowAdapter
+    {
+        private Activity m_context;
+        private View m_View;
+        private Marker m_currentMarker;
+
+        public CustomMapInfoWindow(Activity activity)
+        {
+            m_context = activity;
+            m_View = m_context.LayoutInflater.Inflate(Resource.Layout.custom_infowindow, null);
+        }
+        public View GetInfoContents(Marker marker)
+        {
+            return null;
+        }
+
+        public View GetInfoWindow(Marker marker)
+        {
+            if (marker == null)
+                return null;
+
+            if (marker.Id != "Marker103")
+                return null;
+
+            m_currentMarker = marker;
+
+            ImageView imageview = m_View.FindViewById<ImageView>(Resource.Id.customInfoImage);
+            TextView textviewTitle = m_View.FindViewById<TextView>(Resource.Id.customInfoTitle);
+            TextView textviewDescription = m_View.FindViewById<TextView>(Resource.Id.customInfoDescription);
+            RatingBar ratingBar = m_View.FindViewById<RatingBar>(Resource.Id.customInfoRatingBar);
+
+            if (marker.Title != null)
+                imageview.SetImageResource(Resource.Drawable.maplogo);
+
+            textviewTitle.Text = marker.Title;
+
+            textviewDescription.Text = marker.Snippet;
+
+            ratingBar.Rating = 5;
+
+            return m_View;
+        }
+    }
+}
--- HMS_Map_Demo/HMS_Map_Demo/MainActivity.cs
+++ HMS_Map_Demo/HMS_Map_Demo/MainActivity.cs
@@ -13,6 +13,10 @@ using System;
 using Android.Content.PM;
 using Com.Huawei.Agconnect.Config;
 using HmsMapDemo;
+using Android.Support.V4.Content;
+using Com.Huawei.Hms.Maps.Model;
+using Android.Graphics;
+using Com.Huawei.Hms.Maps.Util;
 
 namespace HMS_Map_Demo
 {
@@ -25,13 +29,8 @@ namespace HMS_Map_Demo
 
         private MapView mMapView;
 
-        private static string[] RUNTIME_PERMISSIONS = {
-            Manifest.Permission.WriteExternalStorage,
-            Manifest.Permission.ReadExternalStorage,
-            Manifest.Permission.AccessCoarseLocation,
-            Manifest.Permission.AccessFineLocation,
-            Manifest.Permission.Internet
-    };
+        Button btnPolylineDemo, btnPolygonDemo, btnCircleDemo, btnMarkersDemo, btnOverlayDemo;
+
 
         private static string MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
         protected override void OnCreate(Bundle savedInstanceState)
@@ -44,38 +43,80 @@ namespace HMS_Map_Demo
             config.OverlayWith(new HmsLazyInputStream(this));
             Com.Huawei.Agconnect.AGConnectInstance.Initialize(this);
 
-            if (!hasPermissions(this, RUNTIME_PERMISSIONS))
-            {
-                ActivityCompat.RequestPermissions(this, RUNTIME_PERMISSIONS, 3);
-            }
+            checkPermission(new string[] {
+                Android.Manifest.Permission.WriteExternalStorage,
+                Android.Manifest.Permission.ReadExternalStorage,
+                Android.Manifest.Permission.AccessCoarseLocation,
+                Android.Manifest.Permission.AccessFineLocation,
+                Android.Manifest.Permission.Internet }, 100);
 
             //get mapview instance
-            mMapView = FindViewById<MapView>(Resource.Id.mapView);
+            mMapView = FindViewById<MapView>(Resource.Id.mapview);
+
+            btnMarkersDemo = FindViewById<Button>(Resource.Id.btnMarkersDemo);
+            btnPolylineDemo = FindViewById<Button>(Resource.Id.btnPolylineDemo);
+            btnPolygonDemo = FindViewById<Button>(Resource.Id.btnPolygonDemo);
+            btnCircleDemo = FindViewById<Button>(Resource.Id.btnCircleDemo);
+            btnOverlayDemo = FindViewById<Button>(Resource.Id.btnOverlayDemo);
+
             Bundle mapViewBundle = null;
             if (savedInstanceState != null)
             {
                 mapViewBundle = savedInstanceState.GetBundle(MAPVIEW_BUNDLE_KEY);
             }
-              mMapView.OnCreate(mapViewBundle);
+            mMapView.OnCreate(mapViewBundle);
             //get map instance
             mMapView.GetMapAsync(this);
         }
 
-        private bool hasPermissions(MainActivity context, string[] permissions)
+        public void checkPermission(string[] permissions, int requestCode)
         {
-            if (Build.VERSION.SdkInt >= Build.VERSION_CODES.M && permissions != null)
+            foreach (string permission in permissions)
             {
-                foreach (var permission in permissions)
+                if (ContextCompat.CheckSelfPermission(this, permission) == Permission.Denied)
                 {
-                    if (ActivityCompat.CheckSelfPermission(context, permission) != Permission.Granted)
-                    {
-                        return false;
-                    }
+                    ActivityCompat.RequestPermissions(this, permissions, requestCode);
                 }
             }
-            return true;
+
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+            mMapView.OnStart();
+
+        }
+        protected override void OnPause()
+        {
+            base.OnStart();
+            mMapView.OnPause();
+
+        }
+        protected override void OnResume()
+        {
+            base.OnStart();
+            mMapView.OnResume();
+
+        }
+        protected override void OnDestroy()
+        {
+            base.OnStart();
+            mMapView.OnDestroy();
+
+        }
+        protected override void OnStop()
+        {
+            base.OnStart();
+            mMapView.OnStop();
+
         }
 
+        private void SetUpMap()
+        {
+            MapView mapFragment = FindViewById<MapView>(Resource.Id.mapview);
+
+            mapFragment.GetMapAsync(this);
+        }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -88,27 +129,235 @@ namespace HMS_Map_Demo
             //get map instance in a callback method
             //Log.d(TAG, "onMapReady: ");
             hMap = map;
+
+            hMap.UiSettings.ZoomControlsEnabled = true;
+            hMap.UiSettings.CompassEnabled = true;
+            hMap.UiSettings.MyLocationButtonEnabled = true;
+
+            hMap.MyLocationEnabled = true;
+            // hMap.MapType = HuaweiMap.MapTypeNone;
+
+            hMap.MarkerClick += OnMarkerClick;
+            hMap.MarkerDragStart += OnMarkerDragStart;
+            hMap.MarkerDrag += OnMarkerDrag;
+            hMap.MarkerDragEnd += OnMarkerDragEnd;
+
+            hMap.PolylineClick += OnPolylineClick;
+            hMap.PolygonClick += OnPolygonClick;
+            hMap.CircleClick += OnCircleClick;
+            hMap.GroundOverlayClick += OnGroundOverlayClick;
+
+            hMap.SetInfoWindowAdapter(new CustomMapInfoWindow(this));
+
+            Toast.MakeText(this, "OnMapReady done.", ToastLength.Short).Show();
+
+            MarkersDemo(hMap);
         }
-        protected override void OnStart()
+        private void btnMarkersDemo_Click(object sender, EventArgs e)
         {
-            base.OnStart();
-            mMapView.OnStart();
+            if (hMap != null)
+                MarkersDemo(hMap);
+            else
+                Toast.MakeText(this, "map not ready", ToastLength.Short).Show();
         }
-        protected override void OnStop()
+        private void btnPolylineDemo_Click(object sender, EventArgs e)
         {
-            base.OnStop();
-            mMapView.OnStop();
+            if (hMap != null)
+                PolylineDemo(hMap);
+            else
+                Toast.MakeText(this, "map not ready", ToastLength.Short).Show();
         }
-        protected override void OnDestroy()
+        private void btnPolygonDemo_Click(object sender, EventArgs e)
         {
-            base.OnDestroy();
-            mMapView.OnDestroy();
+            if (hMap != null)
+                PolygonDemo(hMap);
+            else
+                Toast.MakeText(this, "map not ready", ToastLength.Short).Show();
         }
-        protected override void OnPause()
+        private void btnCircleDemo_Click(object sender, EventArgs e)
         {
-            mMapView.OnPause();
-            base.OnPause();
+            if (hMap != null)
+                CircleDemo(hMap);
+            else
+                Toast.MakeText(this, "map not ready", ToastLength.Short).Show();
+        }
+        private void btnOverlayDemo_Click(object sender, EventArgs e)
+        {
+            if (hMap != null)
+                OverlayDemo(hMap);
+            else
+                Toast.MakeText(this, "map not ready", ToastLength.Short).Show();
         }
 
+        private void MarkersDemo(HuaweiMap hMap)
+        {
+            hMap.Clear();
+
+            Marker marker1;
+            MarkerOptions marker1Options = new MarkerOptions()
+                .InvokePosition(new LatLng(41.0083, 28.9784))
+                .InvokeTitle("Marker Title #1")
+                .InvokeSnippet("Marker Desc #1");
+            marker1 = hMap.AddMarker(marker1Options);
+
+            Marker marker2;
+            MarkerOptions marker2Options = new MarkerOptions()
+                .InvokePosition(new LatLng(41.022231, 29.008118))
+                .InvokeTitle("Marker Title #2")
+                .InvokeSnippet("Marker Desc #2");
+            marker2 = hMap.AddMarker(marker2Options);
+
+            Marker marker3;
+            MarkerOptions marker3Options = new MarkerOptions()
+                .InvokePosition(new LatLng(41.005784, 28.997364))
+                .InvokeTitle("Marker Title #3")
+                .InvokeSnippet("Marker Desc #3");
+            Bitmap bitmap1 = ResourceBitmapDescriptor.DrawableToBitmap(this, ContextCompat.GetDrawable(this, Resource.Drawable.markerblue));
+            marker3Options.InvokeIcon(BitmapDescriptorFactory.FromBitmap(bitmap1));
+            marker3 = hMap.AddMarker(marker3Options);
+
+            Marker marker4;
+            MarkerOptions marker4Options = new MarkerOptions()
+                .InvokePosition(new LatLng(41.028435, 28.988186))
+                .InvokeTitle("Marker Title #4")
+                .InvokeSnippet("Marker Desc #4")
+                .Anchor(0.9F, 0.9F)
+                .Draggable(true);
+            Bitmap bitmap2 = ResourceBitmapDescriptor.DrawableToBitmap(this, ContextCompat.GetDrawable(this, Resource.Drawable.marker));
+            marker4Options.InvokeIcon(BitmapDescriptorFactory.FromBitmap(bitmap2));
+            marker4 = hMap.AddMarker(marker4Options);
+
+        }
+        private void PolylineDemo(HuaweiMap hMap)
+        {
+            hMap.Clear();
+
+            Polyline polyline1;
+            PolylineOptions polyline1Options = new PolylineOptions()
+            .Add(new LatLng(41.01929, 28.967267), new LatLng(41.016785, 28.986971), new LatLng(41.001917, 28.978743), new LatLng(41.002298, 28.954132));
+            polyline1Options.InvokeColor(Color.Blue);
+            polyline1Options.InvokeWidth(20);
+            // polyline1Options.InvokeZIndex(2);
+            polyline1Options.Visible(false);
+            polyline1Options.Clickable(true);
+            polyline1 = hMap.AddPolyline(polyline1Options);
+
+
+            Polyline polyline2;
+            PolylineOptions polyline2Options = new PolylineOptions()
+            .Add(new LatLng(41.010410, 28.978511), new LatLng(41.035243, 29.026812), new LatLng(41.022122, 29.00653), new LatLng(41.00415, 29.012449), new LatLng(41.001699, 28.978743));
+            polyline2Options.InvokeColor(Color.Red);
+            polyline1Options.InvokeZIndex(1);
+            polyline2Options.Clickable(true);
+            polyline2 = hMap.AddPolyline(polyline2Options);
+        }
+        private void PolygonDemo(HuaweiMap hMap)
+        {
+            hMap.Clear();
+
+            Polygon polygon1;
+            PolygonOptions polygon1Options = new PolygonOptions()
+            .Add(new LatLng(41.01929, 28.967267), new LatLng(41.016785, 28.986971), new LatLng(41.014623, 28.999753), new LatLng(41.001917, 28.978743), new LatLng(41.002298, 28.954132));
+            polygon1Options.InvokeFillColor(Color.Argb(60, 255, 200, 0));
+            polygon1Options.InvokeStrokeColor(Color.Yellow);
+            polygon1Options.InvokeStrokeWidth(30);
+            polygon1Options.Clickable(true);
+            polygon1Options.InvokeZIndex(2);
+            polygon1 = hMap.AddPolygon(polygon1Options);
+
+            Polygon polygon2;
+            PolygonOptions polygon2Options = new PolygonOptions()
+            .Add(new LatLng(41.035243, 29.026812), new LatLng(41.022122, 29.00653), new LatLng(41.00415, 29.012449), new LatLng(41.001699, 28.978743), new LatLng(41.017384, 28.986827), new LatLng(41.037711, 28.996791));
+            polygon2Options.InvokeFillColor(Color.Argb(60, 0, 0, 255));
+            polygon2Options.InvokeStrokeColor(Color.Blue);
+            polygon2Options.Clickable(true);
+            polygon2 = hMap.AddPolygon(polygon2Options);
+
+        }
+        private void CircleDemo(HuaweiMap hMap)
+        {
+            hMap.Clear();
+
+            Circle circle1;
+            LatLng circle1LatLng = new LatLng(41.01019, 28.974475);
+            CircleOptions circle1Options = new CircleOptions();
+            circle1Options.InvokeCenter(circle1LatLng);
+            circle1Options.InvokeRadius(1600);
+            circle1Options.InvokeStrokeWidth(5);
+            circle1Options.InvokeStrokeColor(Color.Blue);
+            circle1Options.InvokeFillColor(Color.Argb(60, 0, 0, 255));
+            circle1Options.Clickable(true);
+            circle1Options.InvokeZIndex(2);
+            circle1 = hMap.AddCircle(circle1Options);
+
+            Circle circle2;
+            LatLng circle2LatLng = new LatLng(41.01563, 29.052667);
+            CircleOptions circle2Options = new CircleOptions();
+            circle2Options.InvokeCenter(circle2LatLng);
+            circle2Options.InvokeRadius(5000);
+            circle2Options.InvokeStrokeWidth(10);
+            circle2Options.InvokeStrokeColor(Color.OrangeRed);
+            circle2Options.InvokeFillColor(Color.Argb(60, 255, 200, 0));
+            circle2Options.Clickable(true);
+            circle2Options.InvokeZIndex(1);
+            circle2 = hMap.AddCircle(circle2Options);
+
+        }
+        private void OverlayDemo(HuaweiMap hMap)
+        {
+            hMap.Clear();
+
+            GroundOverlay groundOverlay1;
+            BitmapDescriptor descriptor1 = BitmapDescriptorFactory.FromResource(Resource.Drawable.hmslogo);
+            GroundOverlayOptions groundOverlay1Options = new GroundOverlayOptions()
+                .Position(new LatLng(41.010037, 28.9819363), 2400, 2400)
+                .InvokeImage(descriptor1);
+            groundOverlay1Options.Clickable(true);
+            groundOverlay1 = hMap.AddGroundOverlay(groundOverlay1Options);
+
+            GroundOverlay groundOverlay2;
+            BitmapDescriptor descriptor2 = BitmapDescriptorFactory.FromResource(Resource.Drawable.maplogo);
+            GroundOverlayOptions groundOverlay2Options = new GroundOverlayOptions()
+                .Position(new LatLng(41.024204, 29.009406), 2400, 2400)
+                .InvokeImage(descriptor2);
+            groundOverlay2Options.Clickable(true);
+            groundOverlay2 = hMap.AddGroundOverlay(groundOverlay2Options);
+
+
+
+        }
+
+        public void OnMarkerClick(object sender, HuaweiMap.MarkerClickEventArgs e)
+        {
+            Toast.MakeText(this, $"Marker Click Marker ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnMarkerDragStart(object sender, HuaweiMap.MarkerDragStartEventArgs e)
+        {
+            Toast.MakeText(this, $"Drag Start Marker ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnMarkerDrag(object sender, HuaweiMap.MarkerDragEventArgs e)
+        {
+            Toast.MakeText(this, $"Dragging Marker ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnMarkerDragEnd(object sender, HuaweiMap.MarkerDragEndEventArgs e)
+        {
+            Toast.MakeText(this, $"Drag End Marker ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnPolylineClick(object sender, HuaweiMap.PolylineClickEventArgs e)
+        {
+            Toast.MakeText(this, $"Polyline Click Polyline ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnPolygonClick(object sender, HuaweiMap.PolygonClickEventArgs e)
+        {
+            Toast.MakeText(this, $"Polygon Click Polygon ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnCircleClick(object sender, HuaweiMap.CircleClickEventArgs e)
+        {
+            Toast.MakeText(this, $"Circle Click Circle ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
+        public void OnGroundOverlayClick(object sender, HuaweiMap.GroundOverlayClickEventArgs e)
+        {
+            Toast.MakeText(this, $"Overlay Click Overlay ID: {e.P0.Id}", ToastLength.Short).Show();
+        }
     }
-}
+    }
